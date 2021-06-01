d2ffafab8d1fa1c889ffd14db5b8c1313f507aac
Breno <brenovisk7@gmail.com>
2021-06-01 17:48:09 -0300

    seeing if the other APIs also work in the 'teste.py'

--- p3/teste.py
+++ p3/teste.py
@@ -1,13 +1,18 @@
 # realizando testes para ver o funcionamento do metodo age
-
 import requests
+
+r1 = requests.get('http://127.0.0.1:5000/hello')
+print (r1.text)
+
+r2 = requests.get('http://127.0.0.1:5000/recipe?i=chocolate&q=cake')
+print (r2.text)
 
 # o que eu vou passar no post:
 dados ={
   'name': 'Breno Cabral',
   'date': '2021-07-07',
-  'birthdate': '2021-07-07'
+  'birthdate': '2000-07-07'
 }
 
-r = requests.post('http://127.0.0.1:5000/age', json=dados) # passando
-print(r.text) # checando o que ele esta me retornando
+r3 = requests.post('http://127.0.0.1:5000/age', json=dados) # passando
+print(r3.text) # checando o que ele esta me retornando
